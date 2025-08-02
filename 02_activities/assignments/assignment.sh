83c89bf5749aedc351a8ea036f925e9af53f1586
mlopezza <rmlopezza@gmail.com>
2025-08-02 17:58:47 -0500

    Work to clasify clients data

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -63,7 +63,7 @@ rm ./data/raw/rawdata/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-touch 
+touch inventory.txt
 
 ###########################################
 
