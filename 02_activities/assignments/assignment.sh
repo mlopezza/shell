7b1d240d8ece5690535b708014794332f2e7f68c
mlopezza <rmlopezza@gmail.com>
2025-08-02 18:43:57 -0500

    Final Work to organize clients data

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -63,7 +63,8 @@ rm ./data/raw/rawdata/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-touch inventory.txt
+ls ./data/processed > ./data/processed/inventory.txt
+mv ./data/processed/inventory.txt ./data
 
 ###########################################
 
